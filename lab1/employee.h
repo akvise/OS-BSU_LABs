22e625f37eb6067c3f63582c471df7f2b5d0182c
akvise <akvise@gmail.com>
2022-03-26 00:29:49 +0300

    make creator

--- lab1/employee.h
+++ lab1/employee.h
@@ -1,7 +1,19 @@
-struct employee{
+class employee{
+public:
     int num;
     char name[10];
     double hours;
+
+    employee() {
+        num = 0;
+        strcpy(name, "Noname");
+        hours = 0.;
+    }
+    employee(int num_, const char name_[], double hours_) {
+        num = num_;
+        strcpy(name, name_);
+        hours = hours_;
+    }
 
     friend std::ostream& operator<<(std::ostream &out, employee e){
         out << std::setw(20) << e.num
