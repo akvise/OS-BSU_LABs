ef3e26fb77a64d92b76de4a2ce0cd7f691cf3d99
akvise <akvise@gmail.com>
2022-06-17 15:39:02 +0300

    init commit

---
+++ lab5/server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Akim on 6/17/2022.
+//
+
+#include "server.h"
---
+++ lab5/server.h
@@ -0,0 +1,19 @@
+//
+// Created by Akim on 6/17/2022.
+//
+
+#ifndef LAB5_SERVER_H
+#define LAB5_SERVER_H
+
+
+#include <ostream>
+class server {
+public:
+    int num;
+    char name[10];
+    double hours;
+};
+
+
+
+#endif //LAB5_SERVER_H
